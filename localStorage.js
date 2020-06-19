88cb00a7df7ecb107762222ee6859f9814864277
Pochobs <pochobs@hotmail.com>
2020-06-19 04:31:48 -0500

    minuto 45.55 del video.

---
+++ localStorage.js
@@ -0,0 +1,31 @@
+var elements = [];
+function addElement(){
+    if(document.querySelector(".addTxt").value.trim() !=""){
+        elements.push(document.querySelector(".addTxt").value.trim());
+        // alert(elements);
+        display();
+    }
+}
+function display(){
+    document.querySelector(".list").innerHTML = "";
+    for( var i = 0; i < elements.length; i++)
+    document.querySelector(".list").innerHTML += 
+    "<center><div class='element'>"+
+    elements[i]+
+    "<img class='tick' src='https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQPdrNL09nOpjjqaL2btv2RrLQFh-drL61VGsoO6AghWQxe_PDH&usqp=CAU' onclick='strike("
+    +i+
+    ")'><img class='dustbin' src='https://icons.iconarchive.com/icons/papirus-team/papirus-status/512/user-trash-full-icon.png' onclick='del("
+    +i+
+    ")'></div></center>";
+}
+function del(index) {
+    elements.splice(index, 1);
+    display();
+}
+function strike(index){
+    if(elements[index].includes("<strike>")){
+    elements[index] = elements[index].replace("<strike>", "");
+    elements[index] = elements[index].replace("</strike>", "");
+    } else elements[index] = "<strike>" + elements[index] + "</strike>" ;
+    display();
+}
